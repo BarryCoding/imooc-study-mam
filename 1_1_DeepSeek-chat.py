ca45079aac3b517940e6db3dc8ed30f4df6b60c1
AIMan <zxllks@yahoo.com>
2025-11-10 09:45:12 +0800

    uv setup and test DeepSeek API

---
+++ 1_1_DeepSeek-chat.py
@@ -0,0 +1,23 @@
+import os
+
+import dotenv
+import requests
+
+dotenv.load_dotenv()
+
+response = requests.request(
+    "POST",
+    "https://api.deepseek.com/chat/completions",
+    headers={
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": f"Bearer {os.getenv('DEEPSEEK_API_KEY')}",
+    },
+    json={
+        "model": "deepseek-chat",
+        "messages": [{"role": "user", "content": "你好，你是?"}],
+        "stream": False,
+    },
+)
+
+print(response.json())
---
+++ README.md
@@ -0,0 +1,15 @@
+# Study
+
+## Env
+
+```sh
+cd study-code
+nv init --python 3.12
+nv venv
+```
+
+## 1_DeepSeek_API
+
+```sh
+uv add requests python-dotenv
+```
