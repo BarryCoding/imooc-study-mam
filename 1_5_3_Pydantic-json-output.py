804e4a1af4485407d7bcefde3b922e232e7ef60a
AIMan <zxllks@yahoo.com>
2025-11-17 13:56:08 +0800

    1_5_3: json output validation by pydantic

---
+++ 1_5_3_Pydantic-json-output.py
@@ -0,0 +1,59 @@
+import os
+
+import dotenv
+from openai import OpenAI
+from pydantic import BaseModel, Field
+
+dotenv.load_dotenv()
+
+
+class SplitTask(BaseModel):
+    task_count: int = Field(
+        ..., gt=0, le=10, description="拆分的子任务总数，必须大于0且小于等于10"
+    )
+    tasks: list[str] = Field(..., description="拆分的任务列表")
+
+
+client = OpenAI(
+    base_url=os.getenv("DEEPSEEK_BASE_URL"), api_key=os.getenv("DEEPSEEK_API_KEY")
+)
+
+model = "deepseek-chat"
+response_format = {"type": "json_object"}
+
+system_prompt = """用户将提问一个问题，请拆解这个问题为多个串联的小任务，拆解的小任务数量不超过10个，你可以使用任何假设的工具、LLM、代码等。
+并以json格式输出，其中task_count字段代表拆分任务的总数，tasks为拆分的任务数组(tasks数组内的每个元素都是一个字符串，有顺序之分)。
+
+示例输入:
+今天广州的天气怎样?
+
+示例输出:
+{
+    "task_count": 3,
+    "tasks": ["调用浏览器搜索今天的时间", "调用浏览器搜索广州的天气", "综合搜索的结果/内容调用LLM整理答案并回复用户"]
+}
+"""
+
+while True:
+    user_prompt = input("Query: ").strip()
+    if user_prompt.lower() == "quit":
+        break
+
+    messages = [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt},
+    ]
+
+    response = client.chat.completions.create(
+        model=model, messages=messages, response_format=response_format
+    )
+
+    split_task = SplitTask.model_validate_json(response.choices[0].message.content)
+    print("拆解任务数: ", split_task.task_count)
+
+    for idx, task in enumerate(split_task.tasks):
+        print(f"{str(idx + 1).zfill(2)}.{task}")
+
+    print("===============\n")
+
+# user prompt example: 我想去武汉旅游，请帮我制作一个旅游计划
